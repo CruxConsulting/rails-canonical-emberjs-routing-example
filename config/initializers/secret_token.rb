f1278b1102be5d41dd9f36706b09479391e68df2
Julien Palmas <julien.palmas@gmail.com>
2012-07-25 14:47:45 +0200

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCanonicalEmberjsRoutingExample::Application.config.secret_token = '0cc481fbabc652fc34eb9fe8c4417a0c1c3a9e41818378eea6be2857bcdf87fd34578b4ae982abf663d7f509a7cf004e1da57ea00261ea4ecc9463b2b4b76631'
